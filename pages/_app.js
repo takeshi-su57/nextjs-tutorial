f990c051dd2cf8f816d8daccc3d040a5f9c1550d
LucasLeeWork61 <lucas.lee.work61@gmail.com>
2022-08-11 09:59:49 +0900

    first update

---
+++ pages/_app.js
@@ -0,0 +1,8 @@
+import '../styles/global.css'
+
+export default function App({ Component, pageProps }) {
+  console.log("Component", Component)
+  console.log("pageProps", pageProps)
+  
+  return <Component {...pageProps} />
+}
